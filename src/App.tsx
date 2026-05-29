93e640b80519d62a7453d732e169bd620a8f4e69
Odavid66 <149391081+Odavid66@users.noreply.github.com>
2026-05-29 12:31:37 +0100

    feat: Added routing to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { CartItemCard } from './Components/CartItemCard/card'
 
+const basename = import.meta.env.BASE_URL === '/' ? undefined : import.meta.env.BASE_URL.replace(/\/$/, '')
+
 function App() {
 
   return (
     
-      <CartItemCard/>
+    <Router basename={basename}>
+      <Routes>
+        <Route path="/" element={<CartItemCard />} />
+      </Routes>
+    </Router>
     
   )
 }
