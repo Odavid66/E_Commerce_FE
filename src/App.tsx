c5cbe22bf2c53235cfe8e3bd68b741f01927666e
Ezeme Chidubem <cezeme.2200675@stu.cu.edu.ng>
2026-06-02 00:13:10 +0100

    Designed product and home pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { ProductsPage } from './Pages/Customer/Product/ProductPage'
+import { Layout } from './Components/Layout/Layout'
+// import { ProductsPage } from './Pages/Customer/Product/ProductPage'
 import { ProductInfoPage } from './Pages/Customer/Product/ProductInfoPage'
+import { HomePage } from './Pages/Customer/HomePage/HomePage'
 
 const basename = import.meta.env.BASE_URL === '/' ? undefined : import.meta.env.BASE_URL.replace(/\/$/, '')
 
 function App() {
   return (
     <Router basename={basename}>
-      <Routes>
-        <Route path="/" element={<ProductsPage />} />
-        <Route path="/product/:id" element={<ProductInfoPage />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductInfoPage />} />
+        </Routes>
+      </Layout>
     </Router>
   )
 }
