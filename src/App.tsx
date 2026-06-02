cbe3dd65b8dec39b6b17e54616bfde5df3e1c516
Fenitun <babatundebabafemi26@gmail.com>
2026-06-02 16:02:22 +0100

    fix: route issue

--- src/App.tsx
+++ src/App.tsx
@@ -8,24 +8,26 @@ import { ProductInfoPage } from './Pages/Customer/Product/ProductInfoPage'
 import { HomePage } from './Pages/Customer/HomePage/HomePage'
 import { Cart } from './Pages/Customer/Cart/Cart'
 import { Basebutton } from './Components/Button/button'
+import { Customers } from './Pages/Admin/Customers/Customers'
 
 const basename = import.meta.env.BASE_URL === '/' ? undefined : import.meta.env.BASE_URL.replace(/\/$/, '')
 
 function App() {
   return (
     <Router basename={basename}>
-      <Layout>
-        <Routes>
+      <Routes>
+        {/* Routes with Layout */}
+        <Route element={<Layout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:id" element={<ProductInfoPage />} />
-        </Routes>
-      </Layout>
-      <Routes>
-        {/* Auth Routes */}
+          <Route path="/cart" element={<Cart />} />
+        </Route>
+
+        {/* Standalone Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
-        <Route path="/" element={<Cart />} />
         <Route path="/button" element={<Basebutton onClick={() => alert('Button clicked!')}>Click Me</Basebutton>} />
+        <Route path="/admin/customers" element={<Customers />} />
       </Routes>
     </Router>
   )
