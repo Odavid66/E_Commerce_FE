06229592a474d3969d8370298fb840c27e4734e7
Oluwatimilehinishim <Oluwatimilehin@gmail.com>
2026-06-03 16:47:24 +0100

    General settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { CartItemCard } from './Components/CartItemCard/card'
-import { Success } from './Components/Success & Error cards/errormodal'
+import { Generalsettings } from './Components/settings/generalsettings'
 
 const basename = import.meta.env.BASE_URL === '/' ? undefined : import.meta.env.BASE_URL.replace(/\/$/, '')
 
@@ -11,7 +10,7 @@ function App() {
     
     <Router basename={basename}>
       <Routes>
-        <Route path="/" element={<Success />} />
+        <Route path="/" element={<Generalsettings />} />
       </Routes>
     </Router>
     
